4a434f2836df77b13a2a7ead04ad0a60105e732a
Vladislav Mikkoev <vladislav.mikkoev@yandex.ru>
2024-12-21 22:24:54 +0300

    feat: Решить задачу 3
    Сгенерируйте набор целых чисел, найдите количество чисел, которые являются простыми.

---
+++ src/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int start = 2;
+        const int count = 120;
+        
+        var numbers = Enumerable.Range(start, count).OrderBy(number => number).ToList();
+        numbers.Remove(1);
+        
+        int counter = 0;
+        while (true)
+        {
+            int numbersCountBeforeRemove = numbers.Count;
+            var nextNumber = numbers[counter++];
+            numbers.RemoveAll(number => number % nextNumber == 0 && number != nextNumber);
+            if (numbers.Count == numbersCountBeforeRemove)
+            {
+                break;
+            }
+        }
+
+        var output = string.Join(",", numbers);
+        Console.WriteLine(output);
+        Console.ReadLine();
+    }
+}
