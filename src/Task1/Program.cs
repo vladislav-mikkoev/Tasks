c486403316c2d60b1c0206c6121222dc064adf81
Vladislav Mikkoev <vladislav.mikkoev@yandex.ru>
2024-12-21 17:43:33 +0300

    feat: Решить задачу 1
    Дан текстовый файл, в котором записана последовательность целых чисел. Записать в новый файл только четные из них.

---
+++ src/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace Task1;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        const string separator = ",";
+        const string inputFilePath = "input.txt";
+        const string outputFilePath = "output.txt";
+        
+        var input = await File.ReadAllTextAsync(inputFilePath);
+        var evenNumbers = input.Trim()
+            .Split(separator)
+            .Select(number => int.Parse(number.Trim()))
+            .Where(number => number % 2 == 0)
+            .ToArray();
+        var output = string.Join(separator, evenNumbers);
+        await File.WriteAllTextAsync(outputFilePath, output);
+        
+        Console.WriteLine("Программа завершила работу");
+        Console.ReadLine();
+    }
+}
