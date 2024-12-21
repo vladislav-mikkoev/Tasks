b0101f96f9c1e30c4dc42d1c9af9967639664994
Vladislav Mikkoev <vladislav.mikkoev@yandex.ru>
2024-12-21 22:58:57 +0300

    feat: Решить задачу 4
    Сгенерировать набор целых чисел (руками либо автоматически), проверить, образуют ли они
    симметричную последовательность.

---
+++ src/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = [5, 4, 3, 2, 1, 2, 3, 4, 5];
+        bool isSymmetrically = true;
+        for (int start = 0, end = numbers.Length - 1; start <= end; start++, end--)
+        {
+            if (numbers[start] != numbers[end])
+            {
+                isSymmetrically = false;
+                break;
+            }
+        }
+
+        var output = isSymmetrically
+            ? "симметрично"
+            : "асимметрично";
+        
+        Console.WriteLine(output);
+        Console.ReadLine();
+    }
+}
