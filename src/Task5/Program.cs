088273a5349e6a347f392549ae71438d52eb0667
Vladislav Mikkoev <vladislav.mikkoev@yandex.ru>
2024-12-22 09:25:12 +0300

    feat: Решить задачу 5
    Есть текст на английском языке, заканчивающийся точкой (другие символы "." отсутствуют").
    Требуется написать программу, которая будет определять и выводить на экран английскую букву,
    встречающуюся в этом тексте чаще всего, и количество таких букв.
    Строчные и прописные буквы при этом считаются неразличимыми. Если искомых букв несколько,
    то программа должна выводить на экран первую из них по алфавиту.

---
+++ src/Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string input =
+            """
+            In our fast-paced, always-on world, it's easy to overlook the importance of sleep
+            "However, the truth is, a quality sleep regimen is essential for our overall health and well-being
+            "Like diet and exercise, a good night's sleep is a critical pillar of a healthy lifestyle.
+            """;
+
+        var hit = input.Where(char.IsLetter)
+            .GroupBy(char.ToLower)
+            .OrderBy(group => group.Key)
+            .MaxBy(group => group.Count());
+
+        Console.WriteLine($"Символ {hit!.Key}, количество {hit.Count()}");
+        Console.ReadLine();
+    }
+}
